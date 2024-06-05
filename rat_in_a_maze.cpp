c211947aa07e811e71dd36b271c644ba600e5438
Arzoo Bhuker <118011789+Arzoo12222145@users.noreply.github.com>
2024-06-05 18:37:36 +0530

    Add files via upload

---
+++ rat_in_a_maze.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isSafe(int newx, int newy, vector<vector<bool>>& visited, vector<vector<int>>& arr, int n) {
+    if ((newx >= 0 && newx < n) && (newy >= 0 && newy < n) && (visited[newx][newy] != 1) && (arr[newx][newy] == 1)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void solve(int x, int y, vector<vector<int>>& arr, int n, vector<string>& ans, vector<vector<bool>>& visited, string path) {
+    // Base case
+    if (x == n - 1 && y == n - 1) {
+        ans.push_back(path);
+        return;
+    }
+
+    // 4 movements: Down-Left-Right-Up
+    visited[x][y] = 1;
+
+    // Down
+    if (isSafe(x + 1, y, visited, arr, n)) {
+        solve(x + 1, y, arr, n, ans, visited, path + 'D');
+    }
+
+    // Left
+    if (isSafe(x, y - 1, visited, arr, n)) {
+        solve(x, y - 1, arr, n, ans, visited, path + 'L');
+    }
+
+    // Right
+    if (isSafe(x, y + 1, visited, arr, n)) {
+        solve(x, y + 1, arr, n, ans, visited, path + 'R');
+    }
+
+    // Up
+    if (isSafe(x - 1, y, visited, arr, n)) {
+        solve(x - 1, y, arr, n, ans, visited, path + 'U');
+    }
+
+    visited[x][y] = 0;
+}
+
+vector<string> searchMaze(vector<vector<int>>& arr, int n) {
+    vector<string> ans;
+    vector<vector<bool>> visited(n, vector<bool>(n, 0));
+    string path = "";
+    if (arr[0][0] == 0) {
+        return ans;
+    }
+
+    solve(0, 0, arr, n, ans, visited, path);
+    return ans;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<vector<int>> arr(n, vector<int>(n));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> arr[i][j];
+        }
+    }
+
+    vector<string> result = searchMaze(arr, n);
+
+    if (result.empty()) {
+        cout << "No paths found.\n";
+    } else {
+        cout << "Paths:\n";
+        for (const string& path : result) {
+            cout << path << "\n";
+        }
+    }
+
+    return 0;
+}
